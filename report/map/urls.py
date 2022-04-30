543f04d5a63d0159de7ba7f2efce8d6dc87b22f7
rezamnkh79 <mansourikhahreza@gmail.com>
2022-05-01 00:38:10 +0430

    add setting url

--- report/map/urls.py
+++ report/map/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('map/',views.Map),
     path('insert/',views.Insert_info),
     path('data/',views.Data),
-    
+    path('test/',views.test),
+    path('rsrp/',views.RSRP),
+    path('setting/',views.setting),
     
 ]
