1e4b707497b85baf8be2c20b7ad1ccbff389f92d
rezamnkh79 <mansourikhahreza@gmail.com>
2022-05-01 00:41:15 +0430

    add color_param to admin

--- report/map/admin.py
+++ report/map/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Poit_Info,Color
+from .models import Poit_Info,Color,Color_param
 # Register your models here.
 admin.site.register(Poit_Info)
 admin.site.register(Color)
+admin.site.register(Color_param)
