a0de19c880279a07c1c1513df2e2e0d2906ef716
rezamnkh79 <mansourikhahreza@gmail.com>
2022-06-27 23:03:47 +0430

    add new model and rename models

--- report/map/models.py
+++ report/map/models.py
@@ -1,23 +1,36 @@
 
 from operator import truediv
-from statistics import mode
+from statistics import mode, variance
 from django.db import models
 from pkg_resources import require
 
 
-class Poit_Info(models.Model):
-    Time = models.CharField(max_length=300)
+class Point_Info(models.Model):
+    time = models.CharField(max_length=300)
     latitude = models.DecimalField(
-       max_digits=8, decimal_places=6
-    )
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
     longitude = models.DecimalField(
-       max_digits=8, decimal_places=6
-    )
-    Accuracy = models.IntegerField()
-    Code = models.IntegerField()
-    Power = models.IntegerField()
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )  
+   #  accuracy = models.IntegerField()
+    code = models.IntegerField()
+    power = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+    node = models.IntegerField(blank=True,null=True)
+    technology = models.CharField(max_length=50,blank=True,null=True)
+    arfcn = models.IntegerField(blank=True,null=True)
+    plmnId = models.IntegerField(blank=True,null=True)
+    lac = models.IntegerField(blank=True,null=True)
+    color = models.CharField(max_length=100,blank=True,null=True)
+    cellId = models.BigIntegerField(blank=True,null=True)
+    scan = models.CharField(max_length=50,blank=True,null=True)
+    quality = models.DecimalField(
+         max_digits=8, decimal_places=3,blank=True,null=True
+      )
     
-class Color(models.Model):
+class Ranges(models.Model):
    max = models.IntegerField()
    min = models.IntegerField()
    tech = models.CharField(max_length=50,blank=True,null=True)
@@ -36,12 +49,45 @@ class Color(models.Model):
 
 # class Point(models.Model):
 #     location = models.ListCharField(base_field=models.CharField())
-class Color_param(models.Model):
-      paraeter = models.CharField(max_length=100,blank=True,null=True)
+class Color_Info(models.Model):
+
+      parameter = models.CharField(max_length=100,blank=True,null=True)
       name = models.CharField(max_length=100)
       count =models.IntegerField()
-      color_range =  models.OneToOneField("Color",on_delete=models.CASCADE)
+      color_range =  models.OneToOneField("Ranges",on_delete=models.CASCADE)
       distance = models.IntegerField()
       distribution = models.DecimalField(
          max_digits=8, decimal_places=2,blank=True,null=True
       )
+class Static_Info(models.Model):
+   parameter = models.CharField(max_length=100,blank=True,null=True)
+   count = models.IntegerField()
+   mean = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   max = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   min = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   median = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   mode = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   std = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   variance = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   ci_min = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+   ci_max = models.DecimalField(
+         max_digits=8, decimal_places=2,blank=True,null=True
+      )
+
+
