4cdbacb51c20afa5c4128df464d9f4dfda8bda18
rezamnkh79 <mansourikhahreza@gmail.com>
2022-06-27 22:59:48 +0430

    register new models

--- report/map/admin.py
+++ report/map/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Poit_Info,Color,Color_param
+from .models import Point_Info,Ranges,Color_Info,Static_Info
 # Register your models here.
-admin.site.register(Poit_Info)
-admin.site.register(Color)
-admin.site.register(Color_param)
+admin.site.register(Point_Info)
+admin.site.register(Ranges)
+admin.site.register(Color_Info)
+admin.site.register(Static_Info)
